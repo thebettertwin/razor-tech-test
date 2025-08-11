582b169deef12e133d3591d357e3eba861455926
Andrew Morgans <andy@thebettertwin.co.uk>
2025-08-11 19:38:18 +0100

    [TASK-008] Added 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MovieListingPage from "./pages/movie-listing-page/MovieListingPage";
 
 import Home from "./pages/home/Home";
+import NotFound from "./pages/not-found/NotFound";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/movie/:id" element={<MovieListingPage />} />
       {/* Add a NotFound component for unmatched routes */}
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Page not found - lets get you back on track. Return home</p>
+    </>
+  );
+}
+
+export default NotFound;
